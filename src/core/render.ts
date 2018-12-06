8beb49699e72a3e07fa834ad5310ebd0616a8b4e
le <445220716@qq.com>
2018-12-07 07:36:16 +0800

    Add render

---
+++ src/core/render.ts
@@ -0,0 +1,7 @@
+import diff  from '../vdom/diff';
+
+const render: preact.render = (vnode, parent, merge) => {
+  return diff(merge, vnode, {}, false, parent, false);
+};
+
+export default render;
---
+++ src/vdom/diff.ts
@@ -0,0 +1,13 @@
+const diff: preact.diff = (
+  dom,
+  vnode,
+  context,
+  mountAll,
+  parent,
+  componentRoot
+) => {
+  const ret: Element = null;
+  return ret;
+};
+
+export default diff;
